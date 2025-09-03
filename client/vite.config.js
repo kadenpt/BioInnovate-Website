5450fa231d64352fe06a8d26fb7779d095297a0e
kprimora <kadenpt@gmail.com>
2025-09-03 19:58:19 -0400

    Fix vite base path for GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bioinnovate-website/',  // 👈 add this
+  base: '/Bioinnovate-Website/',  // 👈 add this
 })
 
