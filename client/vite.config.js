31311d09118e4541e42137b116afed35c149d1f4
kprimora <kadenpt@gmail.com>
2025-09-03 20:01:27 -0400

    debug

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Bioinnovate-Website/',  // 👈 add this
+  base: '/BioInnovate-Website/',  // 👈 add this
 })
 
